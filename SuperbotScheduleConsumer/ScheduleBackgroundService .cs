6139bb6952ea4840897a4df23ad631927bf2a6bc
Mateusz Kisiel <mateusz.kisiel.mk@gmail.com>
2024-01-19 02:55:25 +0100

    Fixed Scheduler bug

--- SuperbotScheduleConsumer/ScheduleBackgroundService .cs
+++ SuperbotScheduleConsumer/ScheduleBackgroundService .cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SuperBotManagerBase.Attributes;
@@ -13,43 +14,52 @@ namespace SuperbotSchedule
     {
         private readonly ILogger<ScheduleBackgroundService> logger;
         private readonly IServiceProvider serviceProvider;
+        private readonly IConfigurationManager configurationManager;
 
-        public ScheduleBackgroundService(ILogger<ScheduleBackgroundService> logger, IServiceProvider serviceProvider)
+        int intervalMs;
+        public ScheduleBackgroundService(ILogger<ScheduleBackgroundService> logger, IServiceProvider serviceProvider, IConfigurationManager configurationManager)
         {
             this.logger = logger;
             this.serviceProvider = serviceProvider;
+            this.configurationManager = configurationManager;
+            intervalMs = configurationManager?.GetSection("Scheduler")?.GetValue<int>("IntervalMs") ?? 10000;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while(!stoppingToken.IsCancellationRequested)
             {
-                var scope = serviceProvider.CreateScope();
-                using(var uow = scope.ServiceProvider.GetRequiredService<IAppUnitOfWork>())
-                {
-                    var actionService = scope.ServiceProvider.GetRequiredService<IActionService>();
-                    
-                    var currentTime = DateTime.UtcNow;
-                    var schedules = uow.ActionScheduleRepository.GetAll().Where(a => a.Enabled && a.NextRun <= currentTime).ToList();
-                    foreach(var schedule in schedules)
-                    {
-                        await actionService.EnqueueExecution(schedule.ExecutorId, RunStartType.Scheduled);
-                        logger.LogInformation($"Scheduled executor {schedule.ExecutorId} with schedule {schedule.ActionSCheduleName}");
+                using var scope = serviceProvider.CreateScope();
+                using var uow = scope.ServiceProvider.GetRequiredService<IAppUnitOfWork>();
 
-                        if(schedule.Type == ActionScheduleType.Once)
-                        {
-                            schedule.Enabled = false;
-                        }
-                        else
-                        {
-                            schedule.NextRun = currentTime.AddSeconds(schedule.IntervalSec);
-                        }
-                        await uow.ActionScheduleRepository.Update(schedule);
+                var currentTime = DateTime.UtcNow;
+                var schedules = uow.ActionScheduleRepository.GetAll().Where(a => a.Enabled && a.NextRun <= currentTime).ToList();
+                
+
+                IActionService actionService = null;
+                if(schedules.Count >= 1) /// do not connect to rabbitmq if not necessary
+                    actionService = scope.ServiceProvider.GetRequiredService<IActionService>();
+
+                foreach(var schedule in schedules)
+                {
+                    await actionService.EnqueueExecution(schedule.ExecutorId, RunStartType.Scheduled);
+                    logger.LogInformation($"Scheduled executor {schedule.ExecutorId} with schedule {schedule.ActionSCheduleName}");
+
+                    if(schedule.Type == ActionScheduleType.Once)
+                    {
+                        schedule.Enabled = false;
                     }
-                    if(schedules.Count > 0)
-                        await uow.SaveChangesAsync();
-                    await Task.Delay(10000, stoppingToken); /// TODO : make it configurable
+                    else
+                    {
+                        schedule.NextRun = currentTime.AddSeconds(schedule.IntervalSec);
+                    }
+                    await uow.ActionScheduleRepository.Update(schedule);
                 }
+                if(schedules.Count >= 1)
+                    await uow.SaveChangesAsync();
+
+                await Task.Delay(intervalMs, stoppingToken);
+                
             }
         }
     }
