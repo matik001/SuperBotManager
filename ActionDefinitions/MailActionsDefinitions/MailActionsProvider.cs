6c396f0735cd9bffec4e02ed696877f79741877d
Mateusz Kisiel <mateusz.kisiel.mk@gmail.com>
2024-02-04 15:22:12 +0100

    Added send mail definition

--- ActionDefinitions/MailActionsDefinitions/MailActionsProvider.cs
+++ ActionDefinitions/MailActionsDefinitions/MailActionsProvider.cs
@@ -9,9 +9,9 @@ namespace XKomActionsDefinitions
         public static ActionDefinition MailRead { get; } = new ActionDefinition()
         {
             ActionDefinitionQueueName = "mail-read",
-            ActionDefinitionName = "Mail - read one",
+            ActionDefinitionName = "Mail - Read one",
             ActionDefinitionDescription = "Read one mail and mark it as read",
-            ActionDefinitionIcon = "https://play-lh.googleusercontent.com/D1Dz2BjPYev_oyksKXsdtAS66a_2Ql-sklpzTnwR9lqnDG_P5lAJEtfR70FudJ0XMA",
+            ActionDefinitionIcon = "/mail.png",
             ActionDataSchema = new ActionDefinitionSchema()
             {
                 InputSchema = new List<FieldInfo>()
@@ -37,12 +37,13 @@ namespace XKomActionsDefinitions
                     },
                     new FieldInfo("MailBox", FieldType.String, "Mailbox for mails")
                     {
-                        Placeholder = "Enter a mailbox"
+                        Placeholder = "Enter a mailbox",
+                        InitialValue = "Inbox",
                     },
                     new FieldInfo("SearchPhrase", FieldType.String, "Phrase to search")
                     {
-                        InitialValue = "Inbox",
-                        Placeholder = "Enter a phrase"
+                        Placeholder = "Enter a phrase",
+                        IsOptional = true,
                     },
                 },
                 OutputSchema = new List<FieldInfo>()
@@ -50,6 +51,48 @@ namespace XKomActionsDefinitions
                     new FieldInfo("From", FieldType.String, "From whom you got the mail"),
                     new FieldInfo("Subject", FieldType.String, "Subject of the mail"),
                     new FieldInfo("Body", FieldType.String, "Body of the mail"),
+                },
+            },
+            CreatedDate = DateTime.SpecifyKind(new DateTime(2024, 2, 4), DateTimeKind.Utc),
+            ModifiedDate = DateTime.SpecifyKind(new DateTime(2024, 2, 4), DateTimeKind.Utc),
+            PreserveExecutedInputs = true,
+        };
+
+
+        public static ActionDefinition MailSend { get; } = new ActionDefinition()
+        {
+            ActionDefinitionQueueName = "mail-send",
+            ActionDefinitionName = "Mail - Send",
+            ActionDefinitionDescription = "Send a mail",
+            ActionDefinitionIcon = "/mail.png",
+            ActionDataSchema = new ActionDefinitionSchema()
+            {
+                InputSchema = new List<FieldInfo>()
+                {
+                    new FieldInfo("Host", FieldType.String, "Mail server url")
+                    {
+                        Placeholder = "Enter mail server url"
+                    },
+                    new FieldInfo("Port", FieldType.String, "Server port")
+                    {
+                        Placeholder = "Enter a port number"
+                    },
+                    new FieldInfo("SSL", FieldType.Boolean, "Should use SSL?")
+                    {
+                    },
+                    new FieldInfo("Login", FieldType.String, "Login for server")
+                    {
+                        Placeholder = "Enter login"
+                    },
+                    new FieldInfo("Password", FieldType.Secret, "Password for user")
+                    {
+                        Placeholder = "Enter password"
+                    },
+                    new FieldInfo("Subject", FieldType.String, "Subject of the mail"),
+                    new FieldInfo("Body", FieldType.String, "Body of the mail"),
+                },
+                OutputSchema = new List<FieldInfo>()
+                {
                 },
             },
             CreatedDate = DateTime.SpecifyKind(new DateTime(2024, 2, 4), DateTimeKind.Utc),
