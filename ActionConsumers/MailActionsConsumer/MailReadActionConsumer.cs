81ada8c302b22c0d17316b8eab0020ce54f1f907
Mateusz Kisiel <mateusz.kisiel.mk@gmail.com>
2024-02-04 15:06:57 +0100

    Added read mail action

---
+++ ActionConsumers/MailActionsConsumer/MailReadActionConsumer.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using SuperBotManagerBase.Attributes;
+using SuperBotManagerBase.DB;
+using SuperBotManagerBase.RabbitMq.Concreate;
+using SuperBotManagerBase.Services;
+using SuperBotManagerBase.Utils;
+
+namespace MailActionsConsumer
+{
+    public class ReadMailActionInput
+    {
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public bool SSL { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string MailBox { get; set; }
+        public string SearchPhrase { get; set; }
+        public ReadMailActionInput(Dictionary<string, string> fromInput)
+        {
+            Host = fromInput["Host"];
+            Port = fromInput["Port"];
+            SSL = fromInput["SSL"] == "true";
+            Login = fromInput["Login"];
+            Password = fromInput["Password"];
+            MailBox = ConsumersUtils.BuildMessage("MailBox", fromInput);
+            SearchPhrase = ConsumersUtils.BuildMessage("SearchPhrase", fromInput);
+        }
+    }
+    [ServiceActionConsumer("mail-read")]
+    public class MailReadActionConsumer : ActionQueueConsumer
+    {
+
+        public MailReadActionConsumer(ILogger<ActionQueueConsumer> logger, IAppUnitOfWork uow, IActionService actionService) : base(logger, uow, actionService)
+        {
+        }
+
+
+        protected override async Task<Dictionary<string, string>> ExecuteAsync(SuperBotManagerBase.DB.Repositories.Action action)
+        {
+            ReadMailActionInput input = new ReadMailActionInput(action.ActionData.Input);
+
+
+            return new Dictionary<string, string> { { "Response", "response" } };
+        }
+    }
+}
---
+++ ActionDefinitions/MailActionsDefinitions/MailActionsProvider.cs
@@ -0,0 +1,60 @@
+﻿using SuperBotManagerBase.Attributes;
+using SuperBotManagerBase.DB.Repositories;
+
+namespace XKomActionsDefinitions
+{
+    [ActionsDefinitionProvider("Mail")]
+    public class MailActionsProvider
+    {
+        public static ActionDefinition MailRead { get; } = new ActionDefinition()
+        {
+            ActionDefinitionQueueName = "mail-read",
+            ActionDefinitionName = "Mail - read one",
+            ActionDefinitionDescription = "Read one mail and mark it as read",
+            ActionDefinitionIcon = "https://play-lh.googleusercontent.com/D1Dz2BjPYev_oyksKXsdtAS66a_2Ql-sklpzTnwR9lqnDG_P5lAJEtfR70FudJ0XMA",
+            ActionDataSchema = new ActionDefinitionSchema()
+            {
+                InputSchema = new List<FieldInfo>()
+                {
+                    new FieldInfo("Host", FieldType.String, "Mail server url")
+                    {
+                        Placeholder = "Enter mail server url"
+                    },
+                    new FieldInfo("Port", FieldType.String, "Server port")
+                    {
+                        Placeholder = "Enter a port number"
+                    },
+                    new FieldInfo("SSL", FieldType.Boolean, "Should use SSL?")
+                    {
+                    },
+                    new FieldInfo("Login", FieldType.String, "Login for server")
+                    {
+                        Placeholder = "Enter login"
+                    },
+                    new FieldInfo("Password", FieldType.Secret, "Password for user")
+                    {
+                        Placeholder = "Enter password"
+                    },
+                    new FieldInfo("MailBox", FieldType.String, "Mailbox for mails")
+                    {
+                        Placeholder = "Enter a mailbox"
+                    },
+                    new FieldInfo("SearchPhrase", FieldType.String, "Phrase to search")
+                    {
+                        InitialValue = "Inbox",
+                        Placeholder = "Enter a phrase"
+                    },
+                },
+                OutputSchema = new List<FieldInfo>()
+                {
+                    new FieldInfo("From", FieldType.String, "From whom you got the mail"),
+                    new FieldInfo("Subject", FieldType.String, "Subject of the mail"),
+                    new FieldInfo("Body", FieldType.String, "Body of the mail"),
+                },
+            },
+            CreatedDate = DateTime.SpecifyKind(new DateTime(2024, 2, 4), DateTimeKind.Utc),
+            ModifiedDate = DateTime.SpecifyKind(new DateTime(2024, 2, 4), DateTimeKind.Utc),
+            PreserveExecutedInputs = true,
+        };
+    }
+}
